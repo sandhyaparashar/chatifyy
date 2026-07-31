254332d9875d72c6990f4bc017ad195d785dfe89
Sandhya Rani <24bms034@nith.ac.in>
2026-07-31 19:39:56 +0530

    ready for deployment

--- backened/src/main.js
+++ backened/src/main.js
@@ -1,12 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
+
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 3000;
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(PORT, () => console.log("server running on port:" + PORT))
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log("Server running on port:", PORT);
+});
