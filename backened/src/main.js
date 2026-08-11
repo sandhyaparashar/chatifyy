6a98685e6dd0df26a17800522349a70c23d04320
Sandhya Rani <24bms034@nith.ac.in>
2026-08-11 19:28:10 +0530

    sign controller

--- backened/src/main.js
+++ backened/src/main.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json()) //req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -27,5 +29,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(PORT, () => {
-    console.log("Server running on port:", PORT);
+    console.log("Server running on port:", PORT)
+    connectDB()
 });
