9a464d1d5970ce456fcb354983f0920cacc0e482
Sandhya Rani <24bms034@nith.ac.in>
2026-08-20 20:01:16 +0530

    update cors with real vercel url

--- backened/src/main.js
+++ backened/src/main.js
@@ -1,19 +1,22 @@
 import "dotenv/config";
- import express from "express";
- import cookieParser from "cookie-parser";
-
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
-
-
 const app = express();
 
-
+// <-- Final CORS setup with your real URL -->
+app.use(
+  cors({
+    origin: "https://chatifyy-l3md-32ozymaa4-disco5.vercel.app",
+    credentials: true,
+  })
+);
 
 const PORT = ENV.PORT || 5001;
 app.use(express.json()) //req.body
@@ -22,9 +25,7 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
-
-app.listen(PORT, "0.0.0.0",() => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port:", PORT)
     connectDB()
 });
