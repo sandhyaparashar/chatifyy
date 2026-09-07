7cbd47d4cf0a8d938c1e60a3ffd310e048acacec
Sandhya Rani <24bms034@nith.ac.in>
2026-09-07 19:55:19 +0530

    chatpagelayout

--- frontend/src/pages/Sign.jsx
+++ frontend/src/pages/Sign.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import BorderAnimatedContainer from "../components/bordercontainer";
+import BorderAnimatedContainer from "../components/BorderAnimatedcontainer";
 
 import {
   MessageCircleIcon,
